009e642a1af0615818eee9642c6a699b1b59579a
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2019-02-06 20:40:26 +0100

    Initial commit

---
+++ MarcusW.SharpUtils.Gtk/Resources.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Gtk;
+
+namespace MarcusW.SharpUtils.Gtk
+{
+    public static class Resources
+    {
+        public static CssProvider LoadCss(string resourceName)
+        {
+            if (resourceName == null)
+                throw new ArgumentNullException(nameof(resourceName));
+
+            var cssProvider = new CssProvider();
+            using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName) ?? throw new ArgumentException("Resource not found"))
+            using (var streamReader = new StreamReader(resourceStream))
+            {
+                cssProvider.LoadFromData(streamReader.ReadToEnd());
+            }
+
+            return cssProvider;
+        }
+    }
+}
