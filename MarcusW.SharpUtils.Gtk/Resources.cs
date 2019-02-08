b5d3bbbef53f6c4f82728e8814d329e03c89bc47
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2019-02-08 17:58:31 +0100

    Updated assembly loading for Gtk.Resources.LoadStyle

--- MarcusW.SharpUtils.Gtk/Resources.cs
+++ MarcusW.SharpUtils.Gtk/Resources.cs
@@ -7,13 +7,17 @@ namespace MarcusW.SharpUtils.Gtk
 {
     public static class Resources
     {
-        public static CssProvider LoadStyle(string resourceName)
+        public static CssProvider LoadStyle(string resourceName, Assembly sourceAssembly = null)
         {
             if (resourceName == null)
                 throw new ArgumentNullException(nameof(resourceName));
 
+            Assembly startAssembly = sourceAssembly ?? Assembly.GetEntryAssembly();
+            if (startAssembly == null)
+                throw new ArgumentException("Start assembly cannot be automatically retrieved and therefore needs to be passed as an argument manually.", nameof(sourceAssembly));
+
             var cssProvider = new CssProvider();
-            using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName) ?? throw new ArgumentException("Resource not found"))
+            using (Stream resourceStream = startAssembly.GetManifestResourceStream(resourceName) ?? throw new ArgumentException("Resource not found"))
             using (var streamReader = new StreamReader(resourceStream))
             {
                 cssProvider.LoadFromData(streamReader.ReadToEnd());
